0684179760bc74eae9a891c9ab6d711ecadb68c7
Sam5196 <sammedpachore1008@gmail.com>
2022-05-26 17:41:42 +0530

    first commit

---
+++ AllPrimitiveType.java
@@ -0,0 +1,24 @@
+
+public class AllPrimitiveType {
+	static long a;
+	static double b;
+	static char c;
+	static byte d;
+	static boolean e;
+	static short f;
+	static int g;
+	static float h;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Long :"+a);
+		System.out.println("Double :"+b);
+		System.out.println("Char :"+c);
+		System.out.println("Byte :"+d);
+		System.out.println("Boolean :"+e);
+		System.out.println("Short :"+f);
+		System.out.println("Int :"+g);
+		System.out.println("Float :"+h);
+	}
+
+}
---
+++ StringMethod.java
@@ -0,0 +1,16 @@
+
+public class StringMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String n1="100";
+		String n2="200";
+		String n3="100";
+		
+		System.out.println(n1==n2);
+		System.out.println(n1==n3);
+		System.out.println(n3==n2);
+
+	}
+
+}
